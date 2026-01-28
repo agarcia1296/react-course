32096f1f1b1752389282f72d4f81edda5ea21c34
agarcia1296 <agarcia1296@yahoo.com>
2026-01-28 00:14:41 -0800

    Created copy of ecommerce-project as pressplay-project

---
+++ pushplay-project/src/pages/home/Product.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+import { formatMoney } from "../../utils/money.js";
+import { ProductQuantityContainer } from "../../components/ProductQuantityContainer.jsx";
+
+export function Product({ product, loadCart }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const addToCart = async () => {
+    await axios.post("/api/cart-items", {
+      productId: product.id,
+      quantity,
+    });
+    await loadCart();
+  };
+
+  const selectQuantity = (event) => {
+    const quantitySelected = Number(event.target.value);
+    setQuantity(quantitySelected);
+  };
+
+  return (
+    <div className="product-container">
+      <div className="product-image-container">
+        <img className="product-image" src={product.image} />
+      </div>
+
+      <div className="product-name limit-text-to-2-lines">{product.name}</div>
+
+      <div className="product-rating-container">
+        <img
+          className="product-rating-stars"
+          src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+        />
+        <div className="product-rating-count link-primary">
+          {product.rating.count}
+        </div>
+      </div>
+
+      <div className="product-price">{formatMoney(product.priceCents)}</div>
+
+      <ProductQuantityContainer
+        quantity={quantity}
+        selectQuantity={selectQuantity}
+      />
+
+      <div className="product-spacer"></div>
+
+      <div className="added-to-cart">
+        <img src="images/icons/checkmark.png" />
+        Added
+      </div>
+
+      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+        Add to Cart
+      </button>
+    </div>
+  );
+}
